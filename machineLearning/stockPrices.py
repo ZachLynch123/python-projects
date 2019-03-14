208df729ce70a8b4187470e5a79ae364cf5dbafc
Zach <Zachary.Lynch1337@gmail.com>
2019-03-14 11:46:46 -0700

    getting percent changed

--- machineLearning/stockPrices.py
+++ machineLearning/stockPrices.py
@@ -5,4 +5,13 @@ df = quandl.get('WIKI/GOOGL')
 
 # prints all the features. Can have as many features as you want, but
 # make sure to have meaningful ones that actually have something to do with the data 
+
+# df = dataFrame
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
 print(df.head())
