ece95033fecd146d3f50fc56f433c598200063d1
Zach <Zachary.Lynch1337@gmail.com>
2019-03-17 10:39:36 -0700

    Customizing titles, labels, and legends

--- matplotlib/tut1.py
+++ matplotlib/tut1.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
 
 
+x = [1,2,3]
+y = [5,7,4]
+
+x2 = [1,2,3]
+y2 = [10,14,12]
 
 
-plt.plot([1,2,3],[5,7,4])
 
+
+plt.plot(x,y, label='First Line')
+plt.plot(x2,y2, label="Second Line")
+
+plt.xlabel('plot number')
+plt.ylabel('Important var')
+
+plt.title('Interesting graph..\ncheck it out')
+
+plt.legend()
 plt.show()
